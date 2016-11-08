a986b8c19fe1780a535d24ddb220b6773d1da9a9
IctWbcMrGh <ictwbcmailreceiver@gmail.com>
2016-11-08 13:35:31 +0100

    project V1 change V0.1
    first change, stuff’s been added

---
+++ wbc ipad help app/wbc ipad help appUITests/wbc_ipad_help_appUITests.swift
@@ -0,0 +1,36 @@
+//
+//  wbc_ipad_help_appUITests.swift
+//  wbc ipad help appUITests
+//
+//  Created by ICT-Beheer on 08-11-16.
+//  Copyright © 2016 ICT-Beheer WC Zwijndrecht. All rights reserved.
+//
+
+import XCTest
+
+class wbc_ipad_help_appUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
